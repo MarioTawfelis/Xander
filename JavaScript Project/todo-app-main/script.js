64d8f30ef452a0352ddfd3362c5d665dde8553b3
Mario <mario.tawfelis@outlook.com>
2023-04-03 11:19:35 +0100

    Added functionality to display, add and delete tasks

--- JavaScript Project/todo-app-main/script.js
+++ JavaScript Project/todo-app-main/script.js
@@ -10,8 +10,6 @@ const filterComplete = document.querySelector(".filter-completed") // Filter - c
 
 const clearCompleted = document.querySelector(".clear") //Clear completed tasks button
 
-
-
 const LOCAL_STORAGE_LIST = 'task.todos';
 
 let todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST)) || [
@@ -47,7 +45,59 @@ let todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST)) || [
     },
   ]
 
-  const displayTasks = () => {
+// Creates JSON formatted task
+const createTask = content => {
+    //Create a new ID for the new task by incremeneting the total number of current tasks
+    let id = todos.length + 1
+
+    return {id: id.toString(), name:content, completed:false}
+}
+
+// Stores a task locally
+const storeTask = () => {
+    //Get task content from DOM
+    const content = newTaskText.value
+    console.log(content)
+
+    //Create a new task
+    const task = createTask(content)
+    console.log(task)
+
+    //Append task to JSON var
+    todos.push(task)
+
+    //Store the updated list locally
+    saveLocal()
+
+    //Display tasks
+    displayTasks()
+}
+
+// Deletes a task by ID
+const deleteTask = event => {
+    // Ensure that the delete button was pressed before deletion
+    if(event.target.tagName.toLowerCase() === 'img'){
+        // Get the click button's ID to know which task to delete
+        let taskId = event.target.id
+        // console.log(taskId)
+
+        // Delete selected task
+        todos.splice(Number(taskId)-1, 1)
+    }
+
+    //Store the updated list locally
+    saveLocal()
+
+    //Clear tasks template
+    clearTemplate(tasksContainer)
+
+    //Display tasks
+    displayTasks()
+
+}
+
+// Display tasks by populating the template tag
+const displayTasks = () => {
     todos.forEach(todo => {
         //Clone tasks template
         const tasksList = document.importNode(taskTemplate.content, true)
@@ -55,23 +105,89 @@ let todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST)) || [
         // Get checkbox
         const checkbox = tasksList.querySelector("input")
         checkbox.checked = todo.completed
+        checkbox.id = todo.id
 
         // Add task text
         const text = tasksList.querySelector("label")
         text.append(todo.name)
-
+        text.htmlFor = todo.id
 
         // Add X button to delete
         const deleteBtn = tasksList.querySelector("img")
+        deleteBtn.id = todo.id
 
         tasksContainer.appendChild(tasksList)
     });
-  }
+}
 
+// Clear the template - Invoke before disaplyTasks
+const clearTemplate = container => {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+      }
+}
 
-  function init() {
+// Clears completed tasks
+const clearCompletedTasks = () => {
+    // Filter out completed tasks
+    const incompleteTasks = todos.filter(task => !task.completed)
+    
+    // Update todos
+    todos = incompleteTasks
+
+    //Clear tasks template
+    clearTemplate(tasksContainer)
+
+    //Store the updated list locally
+    saveLocal()
+
+    //Display tasks
     displayTasks()
-  }
+}
 
-  init()
+// Marks a task as (in)complete
+const setComplete = event => {
+    if(event.target.tagName.toLowerCase() === 'input'){
+        // Retrieve the clicked task
+        const seletecdTask = todos.find(task => event.target.id === task.id)
+        seletecdTask.completed = event.target.checked
+    }
+
+    //Clear tasks template
+    clearTemplate(tasksContainer)
+
+    //Store the updated list locally
+    saveLocal()
+
+    //Display tasks
+    displayTasks()
+
+    // Iterate over todos and update 
+    // for(let i=0; i<todos.length-1; i++) {
+    //     if(todos[i].id == taskId) {
+    //         todos[i].completed = true
+    //         break
+    //     }
+    // }
+}
+
+// Store tasks locally
+const saveLocal = () => {
+    localStorage.setItem(LOCAL_STORAGE_LIST, JSON.stringify(todos))
+}
+
+function init() {
+    clearTemplate(tasksContainer)
+    displayTasks()
+    saveLocal()
+}
+
+
+// Event listeners
+addNewForm.addEventListener('submit', storeTask)
+tasksContainer.addEventListener('click', deleteTask)
+tasksContainer.addEventListener('click', setComplete)
+clearCompleted.addEventListener('click', clearCompletedTasks)
+
+init()
 
